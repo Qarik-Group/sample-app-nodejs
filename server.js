1e021afdb4b34ec8d744b230804f2557e10469b8
Dr Nic Williams <drnicwilliams@gmail.com>
2019-09-18 09:40:24 -0400

    Revert "demo - hello s&w"
    This reverts commit dc1539071d2d18a2677e08baf4c6b49ec111ffee.

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => res.send('Hello S&W!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
